b01272ceab98aa555b0088283b0f2655fa1565c6
alessandrovisentini <mail@alessandrovisentini.com>
2026-01-23 19:16:10 +0100

    fix zenmode

--- config/nvim/lua/custom/plugins/zenmode.lua
+++ config/nvim/lua/custom/plugins/zenmode.lua
@@ -25,8 +25,43 @@ return {
       gitsigns = { enabled = false },
       tmux = { enabled = false },
     },
+    on_open = function()
+      -- Apply zen settings to any buffer opened while in zen mode
+      vim.api.nvim_create_augroup('ZenModeBufferSwitch', { clear = true })
+      vim.api.nvim_create_autocmd('BufEnter', {
+        group = 'ZenModeBufferSwitch',
+        callback = function()
+          vim.opt_local.number = false
+          vim.opt_local.relativenumber = false
+          vim.opt_local.signcolumn = 'no'
+          vim.opt_local.cursorline = false
+          vim.opt_local.foldcolumn = '0'
+        end,
+      })
+    end,
+    on_close = function()
+      -- Remove the autocmd when exiting zen mode
+      vim.api.nvim_del_augroup_by_name('ZenModeBufferSwitch')
+    end,
   },
   keys = {
-    { '<leader>z', '<cmd>ZenMode<CR>', desc = '[Z]en mode toggle' },
+    {
+      '<leader>z',
+      function()
+        local zen = require('zen-mode')
+        local view = zen.view
+        -- If zen mode is open, close it and stay on current buffer
+        if view and view.win and vim.api.nvim_win_is_valid(view.win) then
+          local current_buf = vim.api.nvim_get_current_buf()
+          zen.close()
+          vim.schedule(function()
+            vim.api.nvim_set_current_buf(current_buf)
+          end)
+        else
+          zen.open()
+        end
+      end,
+      desc = '[Z]en mode toggle',
+    },
   },
 }
