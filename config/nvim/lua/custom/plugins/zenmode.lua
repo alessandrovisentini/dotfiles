8ccce5c347192ed9e077e74c72212d748d2dcbb6
alessandrovisentini <mail@alessandrovisentini.com>
2026-02-03 10:15:39 +0100

    zendmode in spliview

--- config/nvim/lua/custom/plugins/zenmode.lua
+++ config/nvim/lua/custom/plugins/zenmode.lua
@@ -27,26 +27,41 @@ return {
       tmux = { enabled = false },
     },
     on_open = function(win)
-      -- Reposition zen window and backdrop to leave space for tabline
-      vim.schedule(function()
-        -- Find and reposition all zen-mode floating windows
+      -- Function to reposition zen windows for tabline space
+      local function reposition_zen_windows()
         for _, w in ipairs(vim.api.nvim_list_wins()) do
           local config = vim.api.nvim_win_get_config(w)
-          if config.relative ~= '' then -- It's a floating window
+          if config.relative ~= '' then
             local buf = vim.api.nvim_win_get_buf(w)
             local ft = vim.bo[buf].filetype
-            -- Reposition zen windows (main window or backdrop)
             if ft == 'zenmode-bg' or w == win then
-              config.row = (config.row or 0) + 1
-              config.height = config.height - 1
-              vim.api.nvim_win_set_config(w, config)
+              -- Calculate proper position: row 1 (after tabline), full remaining height
+              local ui = vim.api.nvim_list_uis()[1]
+              if ui then
+                config.row = 1
+                config.height = ui.height - 2 -- Leave space for tabline
+                vim.api.nvim_win_set_config(w, config)
+              end
             end
           end
         end
-      end)
+      end
+
+      -- Initial reposition
+      vim.schedule(reposition_zen_windows)
+
+      -- Create autocmd group
+      vim.api.nvim_create_augroup('ZenModeBufferSwitch', { clear = true })
+
+      -- Reposition on resize
+      vim.api.nvim_create_autocmd('VimResized', {
+        group = 'ZenModeBufferSwitch',
+        callback = function()
+          vim.schedule(reposition_zen_windows)
+        end,
+      })
 
       -- Apply zen settings to any buffer opened while in zen mode
-      vim.api.nvim_create_augroup('ZenModeBufferSwitch', { clear = true })
       vim.api.nvim_create_autocmd('BufEnter', {
         group = 'ZenModeBufferSwitch',
         callback = function()
@@ -57,10 +72,25 @@ return {
           vim.opt_local.foldcolumn = '0'
         end,
       })
+
+      -- Override buffer navigation to work in zen mode floating window
+      for i = 1, 9 do
+        vim.keymap.set('n', '<leader>' .. i, function()
+          local buffers = vim.fn.getbufinfo({ buflisted = 1 })
+          if buffers[i] and vim.api.nvim_win_is_valid(win) then
+            vim.api.nvim_win_set_buf(win, buffers[i].bufnr)
+          end
+        end, { desc = 'Buffer ' .. i })
+      end
     end,
     on_close = function()
-      -- Remove the autocmd when exiting zen mode
+      -- Remove the autocmd group
       vim.api.nvim_del_augroup_by_name('ZenModeBufferSwitch')
+
+      -- Restore original buffer navigation keymaps
+      for i = 1, 9 do
+        vim.keymap.set('n', '<leader>' .. i, '<cmd>BufferLineGoToBuffer ' .. i .. '<cr>', { desc = 'Buffer ' .. i })
+      end
     end,
   },
   keys = {
@@ -69,7 +99,6 @@ return {
       function()
         local zen = require('zen-mode')
         local view = zen.view
-        -- If zen mode is open, close it and stay on current buffer
         if view and view.win and vim.api.nvim_win_is_valid(view.win) then
           local current_buf = vim.api.nvim_get_current_buf()
           zen.close()
