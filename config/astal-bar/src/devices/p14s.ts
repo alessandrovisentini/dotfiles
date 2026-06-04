53446be94a405f542808e8f61c5bb17726f9e261
alessandrovisentini <mail@alessandrovisentini.com>
2026-06-04 17:55:51 +0200

    p14 audio

--- config/astal-bar/src/devices/p14s.ts
+++ config/astal-bar/src/devices/p14s.ts
@@ -2,5 +2,6 @@
 import type { DeviceConfig } from "../types/device"
 
 export const DEVICE: DeviceConfig = {
-  integratedAudioPattern: /Smart Sound|HDA Intel|HD Audio|Tiger Lake|Realtek/i,
+  integratedAudioPattern:
+    /Smart Sound|HDA Intel|HD Audio|Tiger Lake|Raptor Lake|cAVS|Realtek/i,
 }
